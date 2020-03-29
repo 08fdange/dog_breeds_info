78478b7a722357b8687f706170078acca92ee46e
Frank  D'Angelo <08fdange@gmail.com>
2020-03-29 19:27:00 +0000

    added 'get_breed_letter'  method to CLI class

--- lib/dog_breeds_info/cli.rb
+++ lib/dog_breeds_info/cli.rb
@@ -1,5 +1,12 @@
 class DogBreedsInfo::CLI 
+  
   def call
     puts "Test"
   end
+  
+  def get_breed_letter
+    puts "Hello, please enter the first letter of the breed you'd like more information on using 'a-z'"
+  end
+  
+  
 end

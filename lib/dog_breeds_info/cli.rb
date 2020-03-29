0b033b96ff354b6575ed4a62063d85c1a33c9980
Frank  D'Angelo <08fdange@gmail.com>
2020-03-29 19:42:38 +0000

    add function to cli class method 'get_breed_letter'

--- lib/dog_breeds_info/cli.rb
+++ lib/dog_breeds_info/cli.rb
@@ -1,11 +1,16 @@
 class DogBreedsInfo::CLI 
   
   def call
-    puts "Test"
+    get_breed_letter
   end
   
   def get_breed_letter
-    puts "Hello, please enter the first letter of the breed you'd like more information on using 'a-z'"
+    puts "Hello, please enter the first letter of the breed you'd like more information on using 'a-z' or type 'exit': "
+    input = nil 
+    while input != "exit"
+      input = gets.chomp
+      puts "All breeds with the letter #{input}:"
+    end
   end
   
   
